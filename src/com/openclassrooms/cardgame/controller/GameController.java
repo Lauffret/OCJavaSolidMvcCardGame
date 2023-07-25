b20a463f8bf2efc1a5cfc3d564a32c9ac9e5eec0
Lauffret <auffretlea@orange.fr>
2023-07-25 19:20:52 +0200

    Projet fini

---
+++ src/com/openclassrooms/cardgame/controller/GameController.java
@@ -0,0 +1,114 @@
+package com.openclassrooms.cardgame.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.openclassrooms.cardgame.games.GameEvaluator;
+import com.openclassrooms.cardgame.model.Deck;
+import com.openclassrooms.cardgame.model.IPlayer;
+import com.openclassrooms.cardgame.model.Player;
+import com.openclassrooms.cardgame.model.PlayingCard;
+import com.openclassrooms.cardgame.model.WinningPlayer;
+import com.openclassrooms.cardgame.view.GameViewable;
+
+public class GameController {
+
+	enum GameState {
+		AddingPlayers, CardsDealt, WinnerRevealed;
+	}
+
+	Deck deck;
+	List<IPlayer> players;
+	IPlayer winner;
+	GameViewable view;
+	GameState gameState;
+	GameEvaluator evaluator;
+
+	public GameController(Deck deck, GameViewable view, GameEvaluator evaluator) {
+		super();
+		this.deck = deck;
+		this.view = view;
+		this.players = new ArrayList<IPlayer>();
+		this.gameState = GameState.AddingPlayers;
+		this.evaluator = evaluator;
+		view.setController(this);
+	}
+
+	public void run() {
+		while (gameState == GameState.AddingPlayers) {
+			view.promptForPlayerName();
+		}
+
+		switch (gameState) {
+		case CardsDealt:
+			view.promptForFlip();
+			break;
+		case WinnerRevealed:
+			view.promptForNewGame();
+			break;
+		}
+	}
+
+	public void addPlayer(String playerName) {
+		if (gameState == GameState.AddingPlayers) {
+			players.add(new Player(playerName));
+			view.showPlayerName(players.size(), playerName);
+		}
+	}
+
+	public void startGame() {
+		if (gameState != GameState.CardsDealt) {
+			deck.shuffle();
+			int playerIndex = 1;
+			for (IPlayer player : players) {
+				player.addCardToHand(deck.removeTopCard());
+				view.showFaceDownCardForPlayer(playerIndex++, player.getName());
+			}
+			gameState = GameState.CardsDealt;
+		}
+		this.run();
+	}
+	
+	public void exitGame() {
+		System.exit(0);
+	}
+
+	public void flipCards() {
+		int playerIndex = 1;
+		for (IPlayer player : players) {
+			PlayingCard pc = player.getCard(0);
+			pc.flip();
+			view.showCardForPlayer(playerIndex++, player.getName(), pc.getRank().toString(), pc.getSuit().toString());
+		}
+
+		evaluateWinner();
+		displayWinner();
+		rebuildDeck();
+		gameState = GameState.WinnerRevealed;
+		this.run();
+	}
+
+	void evaluateWinner() {
+		winner = new WinningPlayer(evaluator.evaluateWinner(players));
+	}
+
+	void displayWinner() {
+		view.showWinner(winner.getName());
+	}
+
+	void rebuildDeck() {
+		for (IPlayer player : players) {
+			deck.returnCardToDeck(player.removeCard());
+		}
+	}
+
+	public void nextAction(String nextChoice) {
+		if( "e".equals(nextChoice) ) {
+			exitGame();
+		}else {
+			startGame();
+		}
+		
+	}
+
+}
---
+++ src/com/openclassrooms/cardgame/view/GameViewables.java
@@ -0,0 +1,76 @@
+package com.openclassrooms.cardgame.view;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import com.openclassrooms.cardgame.controller.GameController;
+
+public class GameViewables implements GameViewable{
+	
+	List<GameViewable> views;
+	
+	public GameViewables () {
+		views = new ArrayList<GameViewable> ();
+	}
+	
+	public void addViewable (GameViewable view) {
+		views.add(view);
+	}
+	
+	@Override
+	public void setController(GameController gc) {
+		for(GameViewable view : views) {
+			view.setController(gc);
+		}
+	}
+
+	@Override
+	public void promptForPlayerName() {
+		for(GameViewable view : views) {
+			view.promptForPlayerName();
+		}		
+	}
+
+	@Override
+	public void showPlayerName(int playerIndex, String name) {
+		for(GameViewable view : views) {
+			view.showPlayerName(playerIndex, name);
+		}	
+	}
+
+	@Override
+	public void showFaceDownCardForPlayer(int playerIndex, String name) {
+		for(GameViewable view : views) {
+			view.showFaceDownCardForPlayer(playerIndex, name);
+		}
+	}
+
+	@Override
+	public void promptForFlip() {
+		for(GameViewable view : views) {
+			view.promptForFlip();
+		}
+	}
+
+	@Override
+	public void showCardForPlayer(int playerIndex, String name, String rank, String suit) {
+		for(GameViewable view : views) {
+			view.showCardForPlayer(playerIndex, name, rank, suit);
+		}
+	}
+
+	@Override
+	public void showWinner(String winnerName) {
+		for(GameViewable view : views) {
+			view.showWinner(winnerName);
+		}
+	}
+
+	@Override
+	public void promptForNewGame() {
+		for(GameViewable view : views) {
+			view.promptForNewGame();
+		}
+	}
+
+}
